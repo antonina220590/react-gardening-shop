12504dafd5ee58b5b72c7ef53ba46034d1875163
antonina220590 <tonya2290@gmail.com>
2025-07-11 09:10:45 +0300

    chore: set up vitest and configure test coverage

---
+++ src/App.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import App from './App';
+
+describe('App component', () => {
+  it('renders correctly', () => {
+    render(<App />);
+    const headingElement = screen.getByTestId('Test');
+    expect(headingElement).toBeInTheDocument();
+  });
+});
