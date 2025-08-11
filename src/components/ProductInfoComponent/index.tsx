a084911f748ade9df9e0b5af0eb46e1468af21fc
Antonina Tyurina <119872378+antonina220590@users.noreply.github.com>
2025-08-11 16:58:36 +0300

    Build single product page (#4)
    * feat: add image, title and quantity components
    
    * feat: add description of the product
    
    * feat: add mobile version for product info component
    
    * feat: add spinner and error components to the single product page

---
+++ src/components/ProductInfoComponent/index.tsx
@@ -0,0 +1,102 @@
+import styles from './ProductInfoComponent.module.css';
+import { useParams } from 'react-router-dom';
+import { useGetProductByIdQuery } from '../../store/api/apiSlice';
+import QuantityCounterComponent from '../CounterComponent';
+import { useState } from 'react';
+import ButtonCard from '../ui/ButtonCard';
+import ErrorLoadComponent from '../ErrorLoadComponent';
+import SpinnerComponent from '../SpinnerComponent';
+export default function ProductInfoComponent() {
+  const { productId } = useParams();
+  const [quantity, setQuantity] = useState(1);
+
+  const {
+    data: productData,
+    isLoading: isProductLoading,
+    isError: isProductError,
+  } = useGetProductByIdQuery(productId!);
+
+  if (isProductLoading) {
+    return <SpinnerComponent />;
+  }
+
+  const product = productData?.[0];
+
+  if (isProductError || !product) {
+    return <ErrorLoadComponent />;
+  }
+
+  const baseUrl = 'http://localhost:3333';
+  const imageUrl = `${baseUrl}${product?.image}`;
+
+  let discount_percent = 0;
+  if (product.discont_price) {
+    discount_percent = Math.round(
+      ((product.price - product.discont_price) / product.price) * 100
+    );
+  }
+
+  const handleDecrement = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  const handleIncrement = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const handleAddToCart = () => {
+    console.log(`Added ${quantity} of ${product.title} to cart`);
+  };
+
+  return (
+    <section className={styles.single_product}>
+      <div className="container">
+        <div className={styles.product_wrapper}>
+          <div className={styles.image_container}>
+            <img
+              src={imageUrl}
+              alt={product.title}
+              className={styles.product_image}
+            ></img>
+          </div>
+          <div className={styles.info_container}>
+            <h1 className={styles.title}>{product.title}</h1>
+            <div className={styles.price_container}>
+              {product.discont_price && (
+                <p className={styles.product_price}>${product.discont_price}</p>
+              )}
+              <div className={styles.discont_container}>
+                <p
+                  className={`${product.discont_price ? styles.product_discounted_price : styles.product_price}`}
+                >
+                  ${product.price}
+                </p>
+                <div className={styles.badge_container}>
+                  {discount_percent > 0 && (
+                    <div className={styles.discount_badge}>
+                      -{discount_percent}%
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+            <div className={styles.actions}>
+              <QuantityCounterComponent
+                quantity={quantity}
+                onDecrement={handleDecrement}
+                onIncrement={handleIncrement}
+              />
+              <ButtonCard text={'Add to card'} onClick={handleAddToCart} />
+            </div>
+            <div className={styles.description_container}>
+              <h2 className={styles.description_title}>Description</h2>
+              <p className={styles.description_text}>{product.description}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
