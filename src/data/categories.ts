ac8f7f2d822de74bb4af4e495251daba69a2cd4b
antonina220590 <tonya2290@gmail.com>
2025-08-29 16:48:19 +0300

    refactor: delete unused placeholders

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -3,32 +3,27 @@ import category2 from '../assets/protectiveProducts.webp';
 import category3 from '../assets/plantingMaterial.webp';
 import category4 from '../assets/tools.webp';
 import category5 from '../assets/pots.webp';
-import placeholder from '../assets/placeholder_categories.png';
 
 export const categoriesMap = [
-  { id: 1, title: 'Fertilizer', image: category1, placeholder: placeholder },
+  { id: 1, title: 'Fertilizer', image: category1 },
   {
     id: 2,
     title: 'Protective products and septic tanks',
     image: category2,
-    placeholder: placeholder,
   },
   {
     id: 3,
     title: 'Planting material	',
     image: category3,
-    placeholder: placeholder,
   },
   {
     id: 4,
     title: 'Tools and equipment',
     image: category4,
-    placeholder: placeholder,
   },
   {
     id: 5,
     title: 'Pots and planters',
     image: category5,
-    placeholder: placeholder,
   },
 ];
